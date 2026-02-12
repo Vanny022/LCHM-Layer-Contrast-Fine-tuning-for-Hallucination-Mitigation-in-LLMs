328d3b1619ab4c0f5f2290078e43119739a783cd
Vanny022 <vannylee1022@gmail.com>
2026-02-12 18:04:43 +0800

    Delete ge_add_opposite.py

--- ge_add_opposite.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("/home/weli0373/DoLa/general_splits_csv/val.csv")
-
-df["oppositeanswer"] = df["hallucination"].apply(lambda x: "no" if str(x).strip().lower() == "yes" else "yes")
-
-df.to_csv("/home/weli0373/DoLa/general_splits_csv/val.csv", index=False)
-
-print("Done")
